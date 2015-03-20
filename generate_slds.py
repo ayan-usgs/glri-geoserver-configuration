c62b542c3a6f892df062bad78d2aa7097bc2d72b
Yan, Andrew N. <ayan@usgs.gov>
2015-03-20 16:07:12 -0500

    made sld generation script more general

--- generate_slds.py
+++ generate_slds.py
@@ -3,13 +3,23 @@ Created on Mar 19, 2015
 
 @author: ayan
 '''
+import os
 from create_sld.glri_sld_creation import create_glri_sld
+from secure_params import ANNUAL_ANIMATION_DIR, MONTHLY_ANIMATION_DIR
 
 
-monthly_animation_dir = 'C:\\Users\\ayan\\Desktop\\tmp\\LKM_Nov2013_monthly_last10yrs\\hru_means'
+# figure out the directory to put the SLDs
+file_path = file_path = os.path.abspath(os.path.dirname(__file__))
+sld_path = os.path.normpath('{0}/files/slds/glri'.format(file_path))
 
+#create annual SLDs
+annual_slds = create_glri_sld(ANNUAL_ANIMATION_DIR, 'polygon',
+                              xml_writepath=sld_path,
+                              pretty_print=True)
 
-monthly_slds = create_glri_sld(monthly_animation_dir, 'polygon', 
-                               xml_writepath='C:\\Users\\ayan\\Desktop\\tmp\\montly_slds',
+# create monthly SLDs
+monthly_slds = create_glri_sld(MONTHLY_ANIMATION_DIR, 'polygon', 
+                               xml_writepath=sld_path,
                                pretty_print=True, sld_name_suffix='monthly'
                                )
+
